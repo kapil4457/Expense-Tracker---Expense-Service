53e3a7e5e7ccfaa37289a4df64ce0ac0c6de9c3b
Kapil Soni <kapilsoni54768161@gmail.com>
2025-08-25 12:04:18 +0530

    Added mock connection tests

--- src/test/java/com/expense/service/ServiceApplicationTests.java
+++ src/test/java/com/expense/service/ServiceApplicationTests.java
@@ -1,13 +1,43 @@
 package com.expense.service;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
 class ServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+		@Container
+		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.3.0")
+				.withDatabaseName("expenseservice")
+				.withUsername("root")
+				.withPassword("password");
+
+		@Container
+		static KafkaContainer kafka = new KafkaContainer(
+				DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
+
+		@DynamicPropertySource
+		static void configureProperties(DynamicPropertyRegistry registry) {
+			registry.add("spring.datasource.url", mysql::getJdbcUrl);
+			registry.add("spring.datasource.username", mysql::getUsername);
+			registry.add("spring.datasource.password", mysql::getPassword);
+			registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+		}
+
+		@Test
+		void contextLoads() {
+			// sanity check that Spring Boot starts with container DB
+			Assert.assertTrue(mysql.isRunning());
+		}
+
 
 }
